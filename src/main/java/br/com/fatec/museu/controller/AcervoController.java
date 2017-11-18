31f73a7da713619d30433d82104aaae6774144c4
Felipe Futema <felipe.fut09@hotmail.com>
2017-11-18 12:38:08 -0200

    Excluir e editar

--- src/main/java/br/com/fatec/museu/controller/AcervoController.java
+++ src/main/java/br/com/fatec/museu/controller/AcervoController.java
@@ -1,16 +1,18 @@
 package br.com.fatec.museu.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fatec.museu.model.Obra;
 import br.com.fatec.museu.repository.Obras;
+import javassist.NotFoundException;
 
 @Controller
 @RequestMapping("/acervo")
@@ -43,6 +45,33 @@ public class AcervoController {
 	public String cadastrarObra(Obra obra) {
 		if(obra != null) {
 			this.obras.save(obra);
+		}
+		
+		return "redirect:/acervo/obras";
+	}
+	
+	@GetMapping("/editar-obra/{id}")
+	public ModelAndView editarObra(@PathVariable Long id) throws NotFoundException {
+		ModelAndView modelAndView = new ModelAndView("editarObra");
+		
+		Optional<Obra> obra = this.obras.findById(id);
+		
+		if(obra.get() == null) {
+			throw new NotFoundException("Nenhuma obra encontrada com esse Id");
+		}
+		
+		modelAndView.addObject(obra.get());
+		
+		return modelAndView;
+	}
+	
+	@PostMapping("/excluir-obra/{id}")
+	public String excluirObra(@PathVariable Long id) throws Exception {
+		
+		if(id != null) {
+			this.obras.deleteById(id);			
+		}else {
+			throw new Exception("Nenhum registro encontrado para o id = " + id);
 		}
 		
 		return "redirect:/acervo/obras";
